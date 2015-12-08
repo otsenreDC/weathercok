04b225456549845f10f04fdb869d89683b124e5e
Ernesto De los Santos Cordero <ernestodlsc@gmail.com>
2015-12-08 09:46:25 -0400

    Google map is set.

--- app/src/main/java/io/bananalabs/weathercok/WindActivity.java
+++ app/src/main/java/io/bananalabs/weathercok/WindActivity.java
@@ -81,6 +81,9 @@ public class WindActivity extends AppCompatActivity {
             case R.id.action_settings:
                 startActivity(new Intent(this, SettingsActivity.class));
                 return true;
+            case R.id.action_map:
+                startActivity(new Intent(this, MapActivity.class));
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
